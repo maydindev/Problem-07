f4667a17354246b75204e6487142c563ce83fe40
maydindev <maydin.dev@outlook.com>
2024-07-25 14:29:42 +0300

    Added props.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Image from 'next/image'
 
 // imageSrc ve name öğelerini prop olarak geçirin
-function Avatar(props) {
+function Avatar({profileText,imageSrc,name,}) {
   return (
     <div className='group block p-8 flex-shrink-0'>
       <div className='flex items-center'>
         <div>
           <Image
             className='inline-block h-12 w-12 rounded-full'
-            src='/photo.png'
+            src={imageSrc}
             alt=''
             width={500}
             height={500}
@@ -16,10 +16,10 @@ function Avatar(props) {
         </div>
         <div className='ml-3'>
           <p className='text-base font-medium text-gray-700 group-hover:text-gray-900'>
-            Tom Cook
+            {name}
           </p>
           <p className='text-sm font-medium text-gray-500 group-hover:text-gray-700'>
-            {props.profileText}
+            {profileText}
           </p>
         </div>
       </div>
@@ -28,5 +28,5 @@ function Avatar(props) {
 }
 
 export default function App() {
-  return <Avatar profileText='Profile bakın' />
+  return <Avatar profileText='Profile bakın' name="Tom Cook" imageSrc="/photo.png"/>
 }
